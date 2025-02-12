d983533cf0e1c0c7d6f5fc8c0960f4dc38d9b8f1
masx200 <masx200@qq.com>
2025-02-12 11:27:36 +0800

    build(docker): 更新镜像构建流程以包含版本号
    - 在构建和保存 Docker 镜像时添加了获取和使用最新版本号的步骤
    - 修改了镜像名称以包含版本号，例如：masx200/docker-squash:1.2.3
    - 更新了文件名以反映版本号，例如：docker-squash-1.2.3.image.tgz
    - 增加了错误检查以确保成功获取版本号

--- build.sh
+++ build.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 set -e
+PACKAGE_NAME="docker-squash"
 
+# 获取最新版本号
+VERSION=$(curl -s https://pypi.org/pypi/$PACKAGE_NAME/json | jq -r '.info.version')
+
+# 检查是否成功获取版本号
+if [ -z "$VERSION" ]; then
+    echo "Failed to fetch version for package: $PACKAGE_NAME"
+    exit 1
+else
+    echo "The latest version of $PACKAGE_NAME is $VERSION"
+fi
 # 构建镜像
 echo "Building Docker image..."
-docker build -t masx200/docker-squash .
+docker build -t "masx200/docker-squash:$VERSION" .
 docker images
 # 保存镜像
 echo "Saving Docker images..."
-docker save masx200/docker-squash | gzip > docker-squash.image.tgz
-# docker save skybro/ubuntu-cn | gzip > skybro.ubuntu-cn.image.tgz
+docker save "masx200/docker-squash:$VERSION" | gzip > "docker-squash-$VERSION.image.tgz"
+
 
 # 清理容器和镜像
 docker builder prune -f -a
